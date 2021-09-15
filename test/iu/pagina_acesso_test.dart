485ca483beb49e31f1bac85183b692846e12a887
Bruno C. Penha <dev@bruno.penha.nom.br>
2021-09-15 13:49:03 -0300

    Refatorado a inicializacao da pagina inicial para um metodo auxiliar

--- test/iu/pagina_acesso_test.dart
+++ test/iu/pagina_acesso_test.dart
@@ -5,12 +5,8 @@ import 'package:questionario/iu/telas/telas.dart';
 void main() {
   testWidgets("Deveria carregar com o estado inicial",
       (WidgetTester widgetTester) async {
-    final pagAcesso = MaterialApp(
-      home: PaginaAcesso(),
-    );
-
-    await widgetTester.pumpWidget(
-        pagAcesso); // É aqui que o componente é carregado para ser testado
+    await carregaPagina(
+        widgetTester); // É aqui que o componente é carregado para ser testado
 
     final emailTextChildren = find.descendant(
         of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
@@ -27,4 +23,19 @@ void main() {
     final botao = widgetTester.widget<RaisedButton>(find.byType(RaisedButton));
     expect(botao.onPressed, null);
   });
+
+  testWidgets("Deveria validar os valores corretos dos campos",
+      (WidgetTester widgetTester) async {
+    await carregaPagina(
+        widgetTester); // É aqui que o componente é carregado para ser testado
+  });
+}
+
+Future carregaPagina(WidgetTester widgetTester) async {
+  final pagAcesso = MaterialApp(
+    home: PaginaAcesso(),
+  );
+
+  await widgetTester.pumpWidget(
+      pagAcesso); // É aqui que o componente é carregado para ser testado
 }
