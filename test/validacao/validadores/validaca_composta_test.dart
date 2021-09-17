29f670c54bdd122296bc72b58752bcb30e0ae5fe
Bruno C. Penha <dev@bruno.penha.nom.br>
2021-09-17 13:10:17 -0300

    Alterado a estrutura inicial do teste

--- test/validacao/validadores/validaca_composta_test.dart
+++ test/validacao/validadores/validaca_composta_test.dart
@@ -18,19 +18,41 @@ class ValidacaoComposta implements Validador {
 class ValidaCamposSimulado extends Mock implements ValidaCampos {}
 
 void main() {
-  test('Deveria retornar null se todos os validadores retorne null ou vazio', () {
-    final validacao1 = ValidaCamposSimulado();
+  ValidacaoComposta sut;
+  ValidaCamposSimulado validacao1;
+  ValidaCamposSimulado validacao2;
+  ValidaCamposSimulado validacao3;
+
+  void validacao1Simulado(String erro) {
+    when(validacao1.valida(any)).thenReturn(erro);
+  }
+
+  void validacao2Simulado(String erro) {
+    when(validacao2.valida(any)).thenReturn(erro);
+  }
+
+  void validacao3Simulado(String erro) {
+    when(validacao3.valida(any)).thenReturn(erro);
+  }
+
+  setUp(() {
+    validacao1 = ValidaCamposSimulado();
     when(validacao1.campo).thenReturn('qualquer_campo');
-    when(validacao1.valida(any)).thenReturn(null);
+    validacao1Simulado(null);
 
-    final validacao2 = ValidaCamposSimulado();
+    validacao2 = ValidaCamposSimulado();
     when(validacao2.campo).thenReturn('qualquer_campo');
-    when(validacao2.valida(any)).thenReturn('');
+    validacao2Simulado(null);
 
-    final sut = ValidacaoComposta([validacao1, validacao2]);
+    validacao3 = ValidaCamposSimulado();
+    when(validacao3.campo).thenReturn('qualquer_campo');
+    validacao3Simulado(null);
 
-    final erro = sut.valida(campo: 'qualquer_campo', valor: 'qualquer_valor');
+    sut = ValidacaoComposta([validacao1, validacao2]);
+  });
 
-    expect(erro, null);
+  test('Deveria retornar null se todos os validadores retorne null ou vazio', () {
+    validacao2Simulado('');
+    expect(sut.valida(campo: 'qualquer_campo', valor: 'qualquer_valor'), null);
   });
 }
