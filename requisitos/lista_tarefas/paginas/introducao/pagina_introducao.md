c31e22f3b591b2be1ccd47d1c1263e4bfe9ab2ec
Bruno C. Penha <dev@bruno.penha.nom.br>
2021-09-28 14:32:42 -0300

    Garante que a PaginaIntroducao chamge carregaContaAtual quando a pagina for carregada

--- requisitos/lista_tarefas/paginas/introducao/pagina_introducao.md
+++ requisitos/lista_tarefas/paginas/introducao/pagina_introducao.md
@@ -2,6 +2,5 @@
 
 > ## Regras
 1. ✅ Mostrar o carregando ao iniciar a pagina
-2. Chamar o carregar dados da conta atual
+2. ✅ Chamar o carregar dados da conta atual
 3. Nevagar para a página correta ao final do carregamento
-4. Fechar streams quando a página for encerrada
--- test/iu/pagina_introducao_test.dart
+++ test/iu/pagina_introducao_test.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:meta/meta.dart';
+import 'package:mockito/mockito.dart';
 
 class PaginaIntroducao extends StatelessWidget {
+  final ApresentadorIntroducao apresentador;
+
+  PaginaIntroducao({@required this.apresentador});
+
   @override
   Widget build(BuildContext context) {
+    apresentador.carregaContaAtual();
     return Scaffold(
       appBar: AppBar(
         title: Text('Bruno Penha'),
@@ -16,11 +23,20 @@ class PaginaIntroducao extends StatelessWidget {
   }
 }
 
+abstract class ApresentadorIntroducao {
+  Future<void> carregaContaAtual();
+}
+
+class ApresentadorIntroducaoSimulado extends Mock implements ApresentadorIntroducao {}
+
 void main() {
+  ApresentadorIntroducaoSimulado apresentador;
+
   Future carregaPagina(WidgetTester widgetTester) async {
+    apresentador = ApresentadorIntroducaoSimulado();
     await widgetTester.pumpWidget(GetMaterialApp(
       initialRoute: '/',
-      getPages: [GetPage(name: '/', page: () => PaginaIntroducao())],
+      getPages: [GetPage(name: '/', page: () => PaginaIntroducao(apresentador: apresentador))],
     ));
   }
 
@@ -30,5 +46,13 @@ void main() {
 
     // Uma vez que foi carregado o componente, esperamos que tenha um Spinner na tela
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('Deveria chamar carregaContaAtual quando a pagina for carregada',
+      (WidgetTester widgetTester) async {
+    // Carregamos a tela
+    await carregaPagina(widgetTester);
+
+    verify(apresentador.carregaContaAtual()).called(1);
   });
 }
