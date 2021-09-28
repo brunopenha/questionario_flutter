ddee9e12d48f7aa22ade14a2208001f54fcedaef
Bruno C. Penha <dev@bruno.penha.nom.br>
2021-09-28 12:05:51 -0300

    Garantimos que CarregaContaAtualLocalmente retorne uma Conta com sucesso.

--- requisitos/casos_uso/carrega_conta_atual_localmente.md
+++ requisitos/casos_uso/carrega_conta_atual_localmente.md
@@ -2,7 +2,7 @@
 
 > ## Caso de Sucesso
 1. ✅ Sistema solicita o token do usuário gravado no Cache Seguro
-2. Sistema retorna os dados da conta do usuário
+2. ✅ Sistema retorna os dados da conta do usuário
 
 > ## Exceção - Falha ao gravar no cache
 1. Sistema retorna uma mensagem de erro inesperado
--- test/dados/casosuso/carrega_conta_atual/carrega_conta_atual_localmente.dart
+++ test/dados/casosuso/carrega_conta_atual/carrega_conta_atual_localmente.dart
@@ -1,14 +1,43 @@
+import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
+import 'package:questionario/dominios/casosuso/casosuso.dart';
+import 'package:questionario/dominios/entidades/entidades.dart';
 import 'package:test/test.dart';
 
+class CarregaContaAtualLocalmente implements CarregaContaAtual {
+  final ObtemCacheArmazenadoComSeguranca obtemCacheArmazenadoComSeguranca;
+
+  CarregaContaAtualLocalmente({@required this.obtemCacheArmazenadoComSeguranca});
+
+  @override
+  Future<Conta> carrega() async {
+    final tokenRetornado = await obtemCacheArmazenadoComSeguranca.obtemComSeguranca('token');
+
+    return Conta(token: tokenRetornado);
+  }
+}
+
+abstract class ObtemCacheArmazenadoComSeguranca {
+  Future<String> obtemComSeguranca(String chave);
+}
+
+class ObtemCacheArmazenadoComSegurancaSimulado extends Mock implements ObtemCacheArmazenadoComSeguranca {}
+
 void main() {
-  ObtemCacheArmazenadoComSegurancaSimulado obtemCacheArmazenadoComSeguranca;
   CarregaContaAtualLocalmente sut;
+  ObtemCacheArmazenadoComSegurancaSimulado obtemCacheArmazenadoComSeguranca;
+  String textoToken;
+
+  void obtemCacheArmazenadoComSegurancaSimulado() {
+    when(obtemCacheArmazenadoComSeguranca.obtemComSeguranca(any)).thenAnswer((_) async => textoToken);
+  }
 
   setUp(() {
     obtemCacheArmazenadoComSeguranca = ObtemCacheArmazenadoComSegurancaSimulado();
     sut = CarregaContaAtualLocalmente(obtemCacheArmazenadoComSeguranca: obtemCacheArmazenadoComSeguranca);
+    textoToken = faker.guid.guid();
+    obtemCacheArmazenadoComSegurancaSimulado();
   });
 
   test('Deveria chamar ObtemCacheArmazenadoComSeguranca com o valor correto', () async {
@@ -16,20 +45,10 @@ void main() {
 
     verify(obtemCacheArmazenadoComSeguranca.obtemComSeguranca('token'));
   });
-}
 
-class CarregaContaAtualLocalmente {
-  final ObtemCacheArmazenadoComSeguranca obtemCacheArmazenadoComSeguranca;
+  test('Deveria retornar a entidade Conta', () async {
+    final conta = await sut.carrega();
 
-  CarregaContaAtualLocalmente({@required this.obtemCacheArmazenadoComSeguranca});
-
-  Future<void> carrega() async {
-    await obtemCacheArmazenadoComSeguranca.obtemComSeguranca('token');
-  }
-}
-
-class ObtemCacheArmazenadoComSegurancaSimulado extends Mock implements ObtemCacheArmazenadoComSeguranca {}
-
-abstract class ObtemCacheArmazenadoComSeguranca {
-  Future<void> obtemComSeguranca(String chave);
+    expect(conta, Conta(token: textoToken));
+  });
 }
