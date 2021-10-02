c65d08b6c151861800cea4e98ceb47f620cba1e7
Bruno C. Penha <dev@bruno.penha.nom.br>
2021-10-02 14:37:21 -0300

    Adicionado o caso de uso AdicionaConta

---
+++ lib/dominios/casosuso/adiciona_conta.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import '../entidades/conta.dart';
+
+abstract class AdicionaConta {
+  Future<Conta> adiciona(ParametrosAdicionaConta parametros);
+}
+
+class ParametrosAdicionaConta extends Equatable {
+  final String nome;
+  final String email;
+  final String senha;
+  final String confirmaSenha;
+
+  ParametrosAdicionaConta(
+      {@required this.nome, @required this.email, @required this.senha, @required this.confirmaSenha});
+
+  @override
+  List get props => [nome, email, senha, confirmaSenha];
+}
